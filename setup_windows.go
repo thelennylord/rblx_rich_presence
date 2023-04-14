50035d89f6ca4d7bb4ce24ebaefef2c6948bd53a
Lenny Lord <32369619+thelennylord@users.noreply.github.com>
2023-04-14 12:26:26 +0530

    FIx incorrect registry path for roblox-player key

--- setup_windows.go
+++ setup_windows.go
@@ -17,26 +17,26 @@ func setup() error {
 	}
 
 	// Create `SOFTWARE\Classes\roblox-player` key and register it as a URL Protocol
-	newK, _, err := registry.CreateKey(registry.CURRENT_USER, `roblox-player`, registry.WRITE)
+	newK, _, err := registry.CreateKey(registry.CURRENT_USER, `SOFTWARE\Classes\roblox-player`, registry.WRITE)
 	if err != nil {
-		return fmt.Errorf("could not create registry key roblox-player: %v", err)
+		return fmt.Errorf(`could not create registry key SOFTWARE\Classes\roblox-player: %v`, err)
 	}
 	defer newK.Close()
 
 	err = newK.SetStringValue("", "URL: Roblox Protocol with Rich Presence")
 	if err != nil {
-		return fmt.Errorf("could not set default value of key roblox-player: %v", err)
+		return fmt.Errorf(`could not set default value of key SOFTWARE\Classes\roblox-player: %v`, err)
 	}
 
 	err = newK.SetStringValue("URL Protocol", "")
 	if err != nil {
-		return fmt.Errorf("could not set value 'URL Protocol' of key roblox-player: %v", err)
+		return fmt.Errorf(`could not set value 'URL Protocol' of key SOFTWARE\Classes\roblox-player: %v`, err)
 	}
 
 	// Create `SOFTWARE\Classes\roblox-player\DefaultIcon` key
-	newK, _, err = registry.CreateKey(registry.CURRENT_USER, `roblox-player\DefaultIcon`, registry.WRITE)
+	newK, _, err = registry.CreateKey(registry.CURRENT_USER, `SOFTWARE\Classes\roblox-player\DefaultIcon`, registry.WRITE)
 	if err != nil {
-		return fmt.Errorf(`could not create registry key roblox-player\DefaultIcon: %v`, err)
+		return fmt.Errorf(`could not create registry key SOFTWARE\Classes\roblox-player\DefaultIcon: %v`, err)
 	}
 	defer newK.Close()
 
@@ -46,15 +46,15 @@ func setup() error {
 	}
 
 	// Create `SOFTWARE\Classes\roblox-player\shell\open\command` key
-	newK, _, err = registry.CreateKey(registry.CURRENT_USER, `roblox-player\shell\open\command`, registry.WRITE)
+	newK, _, err = registry.CreateKey(registry.CURRENT_USER, `SOFTWARE\Classes\roblox-player\shell\open\command`, registry.WRITE)
 	if err != nil {
-		return fmt.Errorf(`could not create registry key roblox-player\shell\open\command: %v`, err)
+		return fmt.Errorf(`could not create registry key SOFTWARE\Classes\roblox-player\shell\open\command: %v`, err)
 	}
 	defer newK.Close()
 
 	err = newK.SetStringValue("", fmt.Sprintf(`"%s" "%%1"`, execPath))
 	if err != nil {
-		return fmt.Errorf(`could not set default value of key roblox-player\shell\open\command: %v`, err)
+		return fmt.Errorf(`could not set default value of key SOFTWARE\Classes\roblox-player\shell\open\command: %v`, err)
 	}
 
 	return nil
